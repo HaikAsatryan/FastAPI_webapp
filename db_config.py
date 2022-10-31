de1df84a44ac7446088663c9dc2259647e38cab4
HaikAsatryan <Haik.asatryan.95@gmail.com>
2022-11-01 01:20:58 +0400

    SqlAlchemy update

---
+++ db_config.py
@@ -0,0 +1,8 @@
+#Not confidential just for testing purpose
+
+host = 'localhost'
+database = 'fastapi'
+user = 'postgres'
+password = 'root'
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://<<username>:<password>@<ip-address/hostname>/<database_name>'
